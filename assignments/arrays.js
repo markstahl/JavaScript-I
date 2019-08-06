7fbe4403571a4e7306aea4859c39d99145fa0b76
mark stahl <markstahl0219@gmail.com>
2019-08-06 02:38:02 -0400

    arrays complete

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -111,10 +111,10 @@ console.log(oldCars.length);
 let BMWAndAudi = [];
 for (let i = 0; i < inventory.length; i++) {
     if(inventory[i].car_make === 'BMW' || inventory[i].car_make === 'Audi') {
-        BMWAndAudi.push(inventory);
+        BMWAndAudi.push(inventory[i]);
     }
 }
-console.log(JSON.stringify(BMWAndAudi.length));
+console.log(JSON.stringify(BMWAndAudi));
 
 
 
